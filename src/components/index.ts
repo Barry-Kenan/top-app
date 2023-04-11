6fd85c701363eadede3d950e35558ac37333d068
unknown <bary.berdiyev.97@mail.ru>
2023-04-11 12:01:56 +0700

    export всех компонент

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,5 @@
 export { default as Htag } from "./Htag/Htag";
 export { default as Button } from "./Button/Button";
+export { default as P } from "./P/P";
+export { default as Tag } from "./Tag/Tag";
+export { default as Rating } from "./Rating/Rating";
