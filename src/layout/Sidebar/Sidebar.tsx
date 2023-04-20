705bedeafaeaff731d43d3dbc28f086062e738ca
Kenan <bary.berdiyev.97@mail.ru>
2023-04-20 11:31:09 +0700

    компонент меню

--- src/layout/Sidebar/Sidebar.tsx
+++ src/layout/Sidebar/Sidebar.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import styles from "./Sidebar.module.scss";
 import { SidebarProps } from "./Sidebar.props";
+import Menu from "../Menu/Menu";
 
 const Sidebar = ({ ...props }: SidebarProps): JSX.Element => {
-    return <div {...props}>Sidebar</div>;
+    return (
+        <div {...props}>
+            <Menu />
+        </div>
+    );
 };
 
 export default Sidebar;
