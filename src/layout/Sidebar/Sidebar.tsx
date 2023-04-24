88e2cd535868bc7602f726ae3bbd5463a0e6fbfa
Kenan <bary.berdiyev.97@mail.ru>
2023-04-24 11:07:49 +0700

    компонент Sidebar

--- src/layout/Sidebar/Sidebar.tsx
+++ src/layout/Sidebar/Sidebar.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import styles from "./Sidebar.module.scss";
 import { SidebarProps } from "./Sidebar.props";
 import Menu from "../Menu/Menu";
+import cn from "classnames";
+import Logo from "../logo.svg";
 
-const Sidebar = ({ ...props }: SidebarProps): JSX.Element => {
+const Sidebar = ({ className, ...props }: SidebarProps): JSX.Element => {
     return (
-        <div {...props}>
+        <div className={cn(className, styles.sidebar)} {...props}>
+            <Logo className={styles.logo} />
+            <div>Поиск</div>
             <Menu />
         </div>
     );
