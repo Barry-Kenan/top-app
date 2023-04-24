b0ed63f9a0aee1d3e65d795c8481d295d89c5f8e
Kenan <bary.berdiyev.97@mail.ru>
2023-04-24 12:08:26 +0700

    компонент сортировки

---
+++ src/components/Sort/Sort.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { SortEnum, SortProps } from "./Sort.props";
+import styles from "./Sort.module.scss";
+import cn from "classnames";
+import SortIcon from "./sort.svg";
+
+const Sort = ({
+    sort,
+    setSort,
+    className,
+    ...props
+}: SortProps): JSX.Element => {
+    return (
+        <div className={cn(styles.sort, className)} {...props}>
+            <div className={styles.sortName} id="sort">
+                Сортировка
+            </div>
+            <button
+                id="rating"
+                onClick={() => setSort(SortEnum.Rating)}
+                className={cn({
+                    [styles.active]: sort == SortEnum.Rating,
+                })}
+            >
+                <SortIcon className={styles.sortIcon} />
+                По рейтингу
+            </button>
+            <button
+                id="price"
+                onClick={() => setSort(SortEnum.Price)}
+                className={cn({
+                    [styles.active]: sort == SortEnum.Price,
+                })}
+            >
+                <SortIcon className={styles.sortIcon} />
+                По&nbsp;цене
+            </button>
+        </div>
+    );
+};
+
+export default Sort;
