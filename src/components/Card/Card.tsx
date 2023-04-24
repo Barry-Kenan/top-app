9001e5065e86a9268199fe488567421cb42f795e
Kenan <bary.berdiyev.97@mail.ru>
2023-04-24 10:56:58 +0700

    компонент Card

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "./Card.module.scss";
+import { CardProps } from "./Card.props";
+import cn from "classnames";
+
+const Card = ({
+    color = "white",
+    children,
+    className,
+    ...props
+}: CardProps): JSX.Element => {
+    return (
+        <div
+            className={cn(styles.card, className, {
+                [styles.blue]: color == "blue",
+            })}
+            {...props}
+        >
+            {children}
+        </div>
+    );
+};
+
+export default Card;
