768b10018df9399def19b28f02b8596d929e0946
Kenan <bary.berdiyev.97@mail.ru>
2023-04-27 09:39:25 +0700

    добавлен search компонент

--- src/layout/Sidebar/Sidebar.tsx
+++ src/layout/Sidebar/Sidebar.tsx
@@ -4,12 +4,13 @@ import { SidebarProps } from "./Sidebar.props";
 import Menu from "../Menu/Menu";
 import cn from "classnames";
 import Logo from "../logo.svg";
+import { Search } from "@/components";
 
 const Sidebar = ({ className, ...props }: SidebarProps): JSX.Element => {
     return (
         <div className={cn(className, styles.sidebar)} {...props}>
             <Logo className={styles.logo} />
-            <div>Поиск</div>
+            <Search />
             <Menu />
         </div>
     );
