887cdbb517f4d4a6fb68c8a1eac453e884511aad
Kenan <bary.berdiyev.97@mail.ru>
2023-04-27 09:37:49 +0700

    компонент textarea

---
+++ src/components/Textarea/Textarea.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./Textarea.module.scss";
+import cn from "classnames";
+import { TextareaProps } from "./Textarea.props";
+
+const Textarea = ({ className, ...props }: TextareaProps): JSX.Element => {
+    return <textarea className={cn(className, styles.textarea)} />;
+};
+
+export default Textarea;
