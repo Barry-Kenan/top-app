0cf4fbe63bddf6a9e8c03eb51c141cfbd94085c6
Kenan <bary.berdiyev.97@mail.ru>
2023-05-02 11:43:39 +0700

    вывод ошибок и передача ref

--- src/components/Textarea/Textarea.tsx
+++ src/components/Textarea/Textarea.tsx
@@ -1,10 +1,32 @@
-import React from "react";
+import { ForwardedRef, forwardRef, memo } from "react";
 import styles from "./Textarea.module.scss";
 import cn from "classnames";
 import { TextareaProps } from "./Textarea.props";
 
-const Textarea = ({ className, ...props }: TextareaProps): JSX.Element => {
-    return <textarea className={cn(className, styles.textarea)} {...props} />;
-};
+const Textarea = memo(
+    forwardRef(
+        (
+            { className, error, ...props }: TextareaProps,
+            ref: ForwardedRef<HTMLTextAreaElement>
+        ): JSX.Element => {
+            return (
+                <div className={cn(styles.textareaWrapper, className)}>
+                    <textarea
+                        className={cn(styles.textarea, {
+                            [styles.error]: error,
+                        })}
+                        ref={ref}
+                        {...props}
+                    />
+                    {error && (
+                        <span role="alert" className={styles.errorMessage}>
+                            {error.message}
+                        </span>
+                    )}
+                </div>
+            );
+        }
+    )
+);
 
 export default Textarea;
