72bb93b20b77919ed890ec8f7eb4eb437220587b
Kenan <bary.berdiyev.97@mail.ru>
2023-05-02 11:43:24 +0700

    вывод ошибок и передача ref

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,10 +1,32 @@
-import React from "react";
+import { ForwardedRef, forwardRef, memo } from "react";
 import styles from "./Input.module.scss";
 import cn from "classnames";
 import { InputProps } from "./Input.props";
 
-const Input = ({ className, ...props }: InputProps): JSX.Element => {
-    return <input className={cn(className, styles.input)} {...props} />;
-};
+const Input = memo(
+    forwardRef(
+        (
+            { className, error, ...props }: InputProps,
+            ref: ForwardedRef<HTMLInputElement>
+        ): JSX.Element => {
+            return (
+                <div className={cn(styles.inputWrapper, className)}>
+                    <input
+                        className={cn(styles.input, {
+                            [styles.error]: error,
+                        })}
+                        ref={ref}
+                        {...props}
+                    />
+                    {error && (
+                        <span role="alert" className={styles.errorMessage}>
+                            {error.message}
+                        </span>
+                    )}
+                </div>
+            );
+        }
+    )
+);
 
 export default Input;
