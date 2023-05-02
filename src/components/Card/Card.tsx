b061db15d18f0a0ad832b3d8e442adf7c34e20c1
Kenan <bary.berdiyev.97@mail.ru>
2023-05-02 11:45:30 +0700

    передача ref

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,24 +1,27 @@
-import React from "react";
+import React, { ForwardedRef, forwardRef, memo } from "react";
 import styles from "./Card.module.scss";
 import { CardProps } from "./Card.props";
 import cn from "classnames";
 
-const Card = ({
-    color = "white",
-    children,
-    className,
-    ...props
-}: CardProps): JSX.Element => {
-    return (
-        <div
-            className={cn(styles.card, className, {
-                [styles.blue]: color == "blue",
-            })}
-            {...props}
-        >
-            {children}
-        </div>
-    );
-};
+const Card = memo(
+    forwardRef(
+        (
+            { color = "white", children, className, ...props }: CardProps,
+            ref: ForwardedRef<HTMLDivElement>
+        ): JSX.Element => {
+            return (
+                <div
+                    className={cn(styles.card, className, {
+                        [styles.blue]: color == "blue",
+                    })}
+                    ref={ref}
+                    {...props}
+                >
+                    {children}
+                </div>
+            );
+        }
+    )
+);
 
 export default Card;
