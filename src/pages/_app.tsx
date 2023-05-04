b56df5664176c6dd82057eff2fd7fc7ef3853241
Kenan <bary.berdiyev.97@mail.ru>
2023-05-04 15:17:02 +0700

    ошибка стилей исправлена

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -17,16 +17,6 @@ export default function App({ Component, pageProps }: AppProps) {
                     content="width=device-width, initial-scale=1"
                 />
                 <link rel="icon" href="/favicon.ico" />
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link
-                    rel="preconnect"
-                    href="https://fonts.gstatic.com"
-                    crossOrigin="anonymous"
-                />
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap"
-                    rel="stylesheet"
-                />
             </Head>
             <Component {...pageProps} />
         </>
